a8a1930bb261095c6db72cc509d7bb97d77b42fd
KamelotWalker <kamelotwalker@outlook.com>
2024-10-01 01:31:35 +0300

    Ignore messages from bot authors and remove watch mode from start script

--- Src/events/Messages/messageCreate.js
+++ Src/events/Messages/messageCreate.js
@@ -6,6 +6,7 @@ export default new Event({
     once: false, // true if you want to listen once only
     run: (__client__, message) => {
 
+        if (message.author.bot) return;
         custom('Message', message.content);
 
     }
